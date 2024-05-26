1de399238a4ee68c804906bd49c3572f321d0bdf
SCEDSO <105200whiterose@gmail.com>
2024-05-26 22:05:45 +0800

    add switchBtn, itinerary page, itinerary popup(partial)

---
+++ itinerary/style.js
@@ -0,0 +1,6 @@
+function toggleDivs() {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    div1.style.display = div1.style.display === "block" ? "none" : "block";
+    div2.style.display = div2.style.display === "block" ? "none" : "block";
+  }
