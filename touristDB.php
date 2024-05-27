cb4d15d5a92bdbaf167d7ddcd4ecafe704c33f4e
SCEDSO <105200whiterose@gmail.com>
2024-05-27 21:36:17 +0800

    add rating and rating_cnt in trips table

--- touristDB.php
+++ touristDB.php
@@ -62,7 +62,9 @@ $sql = "CREATE TABLE IF NOT EXISTS trips (
     transport VARCHAR(255) NOT NULL,
     image_url VARCHAR(255), /*行程封面 */
     userId CHAR(6), /*創建行程的使用者*/
-    total_date INT /*行程的總天數*/
+    total_date INT, /*行程的總天數*/
+    rating CHAR(1), /* 行程評分(限制0-5) */
+    rating_cnt VARCHAR /* 行程評分人數 */
 )";
 
 if(!mysqli_query($conn, $sql)){
