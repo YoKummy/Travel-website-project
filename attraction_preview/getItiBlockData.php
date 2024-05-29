0b63d0ed0df898795e8a219c148e93909fe1e712
SCEDSO <105200whiterose@gmail.com>
2024-05-29 12:32:35 +0800

    add search engine for iti page

---
+++ attraction_preview/getItiBlockData.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: application/json');
+
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "touristDB"; 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+if ($conn->connect_error) { 
+    die("Connection failed: " . $conn->connect_error); 
+} 
+$userId = "S00001";//預設使用者ID，可用session在使用者登入後儲存 
+$sql = "SELECT trip_name, img_url, total_date, average_score FROM trips"; 
+$Result = $conn->query($sql); 
+if ($Result->num_rows > 0) { 
+    $itis = array();
+    while ($row = $Result->fetch_assoc()) { 
+        $iti[] = array(
+            "trip_name" => $row["trip_name"],
+            "img_url" => $row["img_url"],
+            "total_date" => $row["total_date"],
+            "average_score" => $row["average_score"]
+        );
+        array_push($itis, $iti); 
+    } 
+    echo json_encode($itis, JSON_PRETTY_PRINT);
+} else {
+    echo json_encode(array());
+}
+
+?>
--- attraction_preview/search_engine.js
+++ attraction_preview/search_engine.js
@@ -1,5 +1,5 @@
-const itiBlocksTemplate = document.querySelector("[iti-blocks-template]")
-const itiBlocksContainer = document.querySelector("[iti_blocks_container]")
+const itiBlockTemplate = document.querySelector("[iti-block-template]")
+const itiBlockContainer = document.querySelector("[iti_block_container]")
 const searchInput = document.querySelector("[data-search]")
 
 let itis = []
@@ -11,3 +11,29 @@ searchInput.addEventListener("input", e => {
       iti.element.classList.toggle("hide", !isVisible)
     })
   })
+
+function loadItiBlocks() {
+  $.ajax({
+    url: 'getItiBlockData.php',
+    type: 'POST',
+    dataType: 'json',
+    success: function(data) {
+      itis = data.map(iti => {
+        const block = itiBlockTemplate.content.cloneNode(true).children[0]
+        const header = block.querySelector("[data-header]")
+        const img = block.querySelectorAll("[data-img]")
+        const rating = block.querySelector("[data-rating]")
+        const period = block.querySelector("[data-period]")
+        header.textContent = iti.trip_name
+        img.src = iti.img_url /* unfinished need to loop through multiple img tag */
+        rating.textContent = "社群評價: " + iti.average_score + "星"
+        period.textContent = "天數: " + iti.total_date + "天"
+        itiBlockContainer.append(block)
+        return { name: iti.trip_name} /* may add rating and period after */
+      })
+    },
+    /* error: function() {
+        console.log('無法獲取行程資料');
+    } */
+  });
+}
