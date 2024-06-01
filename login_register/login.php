434dec5800ca0644657634cbf80c7c15b53e2ee7
111403541 <mike32665212@gmail.com>
2024-06-01 13:16:10 +0800

    登入驗證

--- login_register/login.php
+++ login_register/login.php
@@ -1,14 +1,35 @@
 <?php
 session_start();
 
+$servername = "localhost"; 
+$username = "root"; 
+$password = "0305"; 
+$dbname = "touristDB"; 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+if ($conn->connect_error) { 
+    die("Connection failed: " . $conn->connect_error); 
+} 
+
 if(isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username']; // 用戶名
     $password = $_POST['password']; // 密碼
 
-    // 假設用戶名和密碼驗證成功
-    $_SESSION['username'] = $username; // 將用戶名儲存在 session 中
-    header("Location: sidebar.php"); // 重定向到 sidebar.php 主頁面檔名
-    exit();
+    $sql = "SELECT password FROM user WHERE uname = '$username'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashed_password = $row['password'];
+        if (password_verify($password, $hashed_password)) {
+            $_SESSION['username'] = $username;
+            header("Location: sidebar.php");
+            exit();
+        } else {
+            echo "帳號或密碼錯誤";
+        }
+    }  
+}
+else{
+    echo "請輸入帳號和密碼";
 }
 ?>
 
